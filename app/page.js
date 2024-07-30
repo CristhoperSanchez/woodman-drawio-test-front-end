1e60a6e92d7b4b0a6848064bd71d78e763aa7cee
CristhoperSanchez <cris.israel.sanchez@gmail.com>
2024-07-30 16:38:45 +0000

    Making sure before I delete local

---
+++ app/page.js
@@ -0,0 +1,166 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Script from "next/script";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.image_wrapper}>
+        <img style={{
+          minHeight: '100px',
+          minWidth: '100px',
+          backgroundColor: 'white'
+        }} className="drawio"
+          src='<mxfile host="Electron" modified="2024-07-16T15:14:48.341Z" agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) draw.io/24.4.0 Chrome/120.0.6099.109 Electron/28.1.0 Safari/537.36" etag="BHN60uERmkViHRWpurLR" version="24.4.0" type="device">
+          <diagram name="Page-1" id="xuF6ZN6XEambxk3Tc71c">
+            <mxGraphModel dx="1674" dy="836" grid="1" gridSize="10" guides="1" tooltips="1" connect="1" arrows="1" fold="1" page="1" pageScale="1" pageWidth="850" pageHeight="1100" math="0" shadow="0">
+              <root>
+                <mxCell id="0" />
+                <mxCell id="1" parent="0" />
+                <object label="" Name="DEFAULT_ASSET" Address="DEFAULT_ADDRESS" Controller="BOS-9" id="GTtxsocZ6iIW7ZSogRPM-15">
+                  <mxCell style="whiteSpace=wrap;html=1;strokeColor=none;allowArrows=0;dropTarget=0;points=[[0.33,0,0,0,0],[0.42,0,0,0,0],[0.5,0,0,0,0],[0.58,0,0,0,0],[0.67,0,0,0,0]];container=0;collapsible=0;metaEdit=1;" vertex="1" parent="1">
+                    <mxGeometry x="50" y="70" width="120" height="100" as="geometry" />
+                  </mxCell>
+                </object>
+                <object placeholder="Address" placeholders="1" label="DEFAULT_ADDRESS" id="GTtxsocZ6iIW7ZSogRPM-16">
+                  <mxCell style="text;align=center;html=1;verticalAlign=middle;whiteSpace=wrap;rounded=0;fillColor=#f5f5f5;fontColor=#333333;strokeColor=#666666;points=[];" vertex="1" parent="GTtxsocZ6iIW7ZSogRPM-15">
+                    <mxGeometry y="80" width="120" height="20" as="geometry" />
+                  </mxCell>
+                </object>
+                <object placeholders="1" placeholder="Name" label="DEFAULT_ASSET" id="GTtxsocZ6iIW7ZSogRPM-17">
+                  <mxCell style="text;align=center;html=1;verticalAlign=middle;whiteSpace=wrap;rounded=0;fillColor=#f5f5f5;fontColor=#333333;strokeColor=#666666;points=[];" vertex="1" parent="GTtxsocZ6iIW7ZSogRPM-15">
+                    <mxGeometry y="60" width="120" height="20" as="geometry" />
+                  </mxCell>
+                </object>
+                <mxCell id="GTtxsocZ6iIW7ZSogRPM-18" value="" style="group;points=[];dropTarget=0;allowArrows=0;fillColor=#f5f5f5;fontColor=#333333;strokeColor=#666666;" vertex="1" connectable="0" parent="GTtxsocZ6iIW7ZSogRPM-15">
+                  <mxGeometry x="20" width="80" height="52" as="geometry" />
+                </mxCell>
+                <mxCell id="GTtxsocZ6iIW7ZSogRPM-19" value="" style="whiteSpace=wrap;html=1;fillColor=#CCCCCC;allowArrows=0;connectable=0;strokeColor=#36393d;" vertex="1" parent="GTtxsocZ6iIW7ZSogRPM-18">
+                  <mxGeometry width="80" height="52" as="geometry" />
+                </mxCell>
+                <mxCell id="GTtxsocZ6iIW7ZSogRPM-20" value="" style="group;points=[];allowArrows=0;dropTarget=0;" vertex="1" connectable="0" parent="GTtxsocZ6iIW7ZSogRPM-18">
+                  <mxGeometry x="10" y="48" width="60" height="4" as="geometry" />
+                </mxCell>
+                <mxCell id="GTtxsocZ6iIW7ZSogRPM-21" value="" style="whiteSpace=wrap;html=1;rotation=0;fillColor=#4D4D4D;connectable=0;allowArrows=0;" vertex="1" parent="GTtxsocZ6iIW7ZSogRPM-20">
+                  <mxGeometry width="60" height="4" as="geometry" />
+                </mxCell>
+                <mxCell id="GTtxsocZ6iIW7ZSogRPM-22" value="" style="edgeStyle=none;orthogonalLoop=1;jettySize=auto;html=1;rounded=0;endArrow=none;endFill=0;" edge="1" parent="GTtxsocZ6iIW7ZSogRPM-20">
+                  <mxGeometry width="80" relative="1" as="geometry">
+                    <mxPoint x="56.00000000000001" y="4" as="sourcePoint" />
+                    <mxPoint x="56.00000000000001" as="targetPoint" />
+                    <Array as="points" />
+                  </mxGeometry>
+                </mxCell>
+                <mxCell id="GTtxsocZ6iIW7ZSogRPM-23" value="" style="edgeStyle=none;orthogonalLoop=1;jettySize=auto;html=1;rounded=0;endArrow=none;endFill=0;" edge="1" parent="GTtxsocZ6iIW7ZSogRPM-20">
+                  <mxGeometry width="80" relative="1" as="geometry">
+                    <mxPoint x="51.99999999999999" y="4" as="sourcePoint" />
+                    <mxPoint x="51.99999999999999" as="targetPoint" />
+                    <Array as="points" />
+                  </mxGeometry>
+                </mxCell>
+                <mxCell id="GTtxsocZ6iIW7ZSogRPM-24" value="" style="edgeStyle=none;orthogonalLoop=1;jettySize=auto;html=1;rounded=0;endArrow=none;endFill=0;" edge="1" parent="GTtxsocZ6iIW7ZSogRPM-20">
+                  <mxGeometry width="80" relative="1" as="geometry">
+                    <mxPoint x="48" y="4" as="sourcePoint" />
+                    <mxPoint x="48" as="targetPoint" />
+                    <Array as="points" />
+                  </mxGeometry>
+                </mxCell>
+                <mxCell id="GTtxsocZ6iIW7ZSogRPM-25" value="" style="edgeStyle=none;orthogonalLoop=1;jettySize=auto;html=1;rounded=0;endArrow=none;endFill=0;" edge="1" parent="GTtxsocZ6iIW7ZSogRPM-20">
+                  <mxGeometry width="80" relative="1" as="geometry">
+                    <mxPoint x="43.99999999999999" y="4" as="sourcePoint" />
+                    <mxPoint x="43.99999999999999" as="targetPoint" />
+                    <Array as="points" />
+                  </mxGeometry>
+                </mxCell>
+                <mxCell id="GTtxsocZ6iIW7ZSogRPM-26" value="" style="edgeStyle=none;orthogonalLoop=1;jettySize=auto;html=1;rounded=0;endArrow=none;endFill=0;" edge="1" parent="GTtxsocZ6iIW7ZSogRPM-20">
+                  <mxGeometry width="80" relative="1" as="geometry">
+                    <mxPoint x="40" y="4" as="sourcePoint" />
+                    <mxPoint x="40" as="targetPoint" />
+                    <Array as="points" />
+                  </mxGeometry>
+                </mxCell>
+                <mxCell id="GTtxsocZ6iIW7ZSogRPM-27" value="" style="edgeStyle=none;orthogonalLoop=1;jettySize=auto;html=1;rounded=0;endArrow=none;endFill=0;" edge="1" parent="GTtxsocZ6iIW7ZSogRPM-20">
+                  <mxGeometry width="80" relative="1" as="geometry">
+                    <mxPoint x="36" y="4" as="sourcePoint" />
+                    <mxPoint x="36" as="targetPoint" />
+                    <Array as="points" />
+                  </mxGeometry>
+                </mxCell>
+                <mxCell id="GTtxsocZ6iIW7ZSogRPM-28" value="" style="edgeStyle=none;orthogonalLoop=1;jettySize=auto;html=1;rounded=0;endArrow=none;endFill=0;" edge="1" parent="GTtxsocZ6iIW7ZSogRPM-20">
+                  <mxGeometry width="80" relative="1" as="geometry">
+                    <mxPoint x="31.999999999999993" y="4" as="sourcePoint" />
+                    <mxPoint x="31.999999999999993" as="targetPoint" />
+                    <Array as="points" />
+                  </mxGeometry>
+                </mxCell>
+                <mxCell id="GTtxsocZ6iIW7ZSogRPM-29" value="" style="edgeStyle=none;orthogonalLoop=1;jettySize=auto;html=1;rounded=0;endArrow=none;endFill=0;" edge="1" parent="GTtxsocZ6iIW7ZSogRPM-20">
+                  <mxGeometry width="80" relative="1" as="geometry">
+                    <mxPoint x="28.000000000000004" y="4" as="sourcePoint" />
+                    <mxPoint x="28.000000000000004" as="targetPoint" />
+                    <Array as="points" />
+                  </mxGeometry>
+                </mxCell>
+                <mxCell id="GTtxsocZ6iIW7ZSogRPM-30" value="" style="edgeStyle=none;orthogonalLoop=1;jettySize=auto;html=1;rounded=0;endArrow=none;endFill=0;" edge="1" parent="GTtxsocZ6iIW7ZSogRPM-20">
+                  <mxGeometry width="80" relative="1" as="geometry">
+                    <mxPoint x="24" y="4" as="sourcePoint" />
+                    <mxPoint x="24" as="targetPoint" />
+                    <Array as="points" />
+                  </mxGeometry>
+                </mxCell>
+                <mxCell id="GTtxsocZ6iIW7ZSogRPM-31" value="" style="edgeStyle=none;orthogonalLoop=1;jettySize=auto;html=1;rounded=0;endArrow=none;endFill=0;" edge="1" parent="GTtxsocZ6iIW7ZSogRPM-20">
+                  <mxGeometry width="80" relative="1" as="geometry">
+                    <mxPoint x="20" y="4" as="sourcePoint" />
+                    <mxPoint x="20" as="targetPoint" />
+                    <Array as="points" />
+                  </mxGeometry>
+                </mxCell>
+                <mxCell id="GTtxsocZ6iIW7ZSogRPM-32" value="" style="edgeStyle=none;orthogonalLoop=1;jettySize=auto;html=1;rounded=0;endArrow=none;endFill=0;" edge="1" parent="GTtxsocZ6iIW7ZSogRPM-20">
+                  <mxGeometry width="80" relative="1" as="geometry">
+                    <mxPoint x="15.999999999999996" y="4" as="sourcePoint" />
+                    <mxPoint x="15.999999999999996" as="targetPoint" />
+                    <Array as="points" />
+                  </mxGeometry>
+                </mxCell>
+                <mxCell id="GTtxsocZ6iIW7ZSogRPM-33" value="" style="edgeStyle=none;orthogonalLoop=1;jettySize=auto;html=1;rounded=0;endArrow=none;endFill=0;" edge="1" parent="GTtxsocZ6iIW7ZSogRPM-20">
+                  <mxGeometry width="80" relative="1" as="geometry">
+                    <mxPoint x="12" y="4" as="sourcePoint" />
+                    <mxPoint x="12" as="targetPoint" />
+                    <Array as="points" />
+                  </mxGeometry>
+                </mxCell>
+                <mxCell id="GTtxsocZ6iIW7ZSogRPM-34" value="" style="edgeStyle=none;orthogonalLoop=1;jettySize=auto;html=1;rounded=0;endArrow=none;endFill=0;" edge="1" parent="GTtxsocZ6iIW7ZSogRPM-20">
+                  <mxGeometry width="80" relative="1" as="geometry">
+                    <mxPoint x="7.999999999999998" y="4" as="sourcePoint" />
+                    <mxPoint x="7.999999999999998" as="targetPoint" />
+                    <Array as="points" />
+                  </mxGeometry>
+                </mxCell>
+                <mxCell id="GTtxsocZ6iIW7ZSogRPM-35" value="" style="edgeStyle=none;orthogonalLoop=1;jettySize=auto;html=1;rounded=0;endArrow=none;endFill=0;" edge="1" parent="GTtxsocZ6iIW7ZSogRPM-20">
+                  <mxGeometry width="80" relative="1" as="geometry">
+                    <mxPoint x="3.999999999999999" y="4" as="sourcePoint" />
+                    <mxPoint x="3.999999999999999" as="targetPoint" />
+                    <Array as="points" />
+                  </mxGeometry>
+                </mxCell>
+                <mxCell id="GTtxsocZ6iIW7ZSogRPM-36" value="" style="whiteSpace=wrap;html=1;fillColor=#B3B3B3;strokeColor=none;points=[];connectable=0;" vertex="1" parent="GTtxsocZ6iIW7ZSogRPM-18">
+                  <mxGeometry x="1.3333333333333333" y="12" width="77.33333333333333" height="4" as="geometry" />
+                </mxCell>
+                <mxCell id="GTtxsocZ6iIW7ZSogRPM-37" value="" style="whiteSpace=wrap;html=1;fillColor=#999999;fontColor=#FFFFFF;strokeColor=none;points=[];connectable=0;" vertex="1" parent="GTtxsocZ6iIW7ZSogRPM-18">
+                  <mxGeometry x="1.3333333333333333" y="36" width="77.33333333333333" height="4" as="geometry" />
+                </mxCell>
+                <object placeholder="Controller" placeholders="1" label="BOS" id="GTtxsocZ6iIW7ZSogRPM-38">
+                  <mxCell style="whiteSpace=wrap;html=1;fillColor=#333333;fontColor=#FFFFFF;points=[];fontSize=10;connectable=0;" vertex="1" parent="GTtxsocZ6iIW7ZSogRPM-18">
+                    <mxGeometry y="16" width="80" height="20" as="geometry" />
+                  </mxCell>
+                </object>
+              </root>
+            </mxGraphModel>
+          </diagram>
+        </mxfile>
+        '
+        />
+      </div>
+      <Script src="/drawio-script.js" strategy="beforeInteractive"/>
+    </main>
+  );
+}
---
+++ public/drawio-script.js
@@ -0,0 +1,64 @@
+
+
+
+document.addEventListener('dblclick', (evt) =>{
+  console.log("Double Click detected")
+    var url = 'https://embed.diagrams.net/?embed=1&ui=atlas&spin=1&modified=unsavedChanges&proto=json';
+    var source = evt.srcElement || evt.target;
+
+    if ( /* source.nodeName == 'IMG' &&  */source.className == 'drawio')
+    {
+      if (source.drawIoWindow == null || source.drawIoWindow.closed)
+      {
+        // Implements protocol for loading and exporting with embedded XML
+        var receive = function(evt)
+        {
+          if (evt.data.length > 0 && evt.source == source.drawIoWindow)
+          {
+            var msg = JSON.parse(evt.data);
+
+            // Received if the editor is ready
+            if (msg.event == 'init')
+            {
+              // Sends the data URI with embedded XML to editor
+              source.drawIoWindow.postMessage(JSON.stringify(
+                { action: 'load', xml: source.getAttribute('src')}), '*');
+            }
+            // Received if the user clicks save
+            else if (msg.event == 'save')
+            {
+              // Sends a request to export the diagram as XML with embedded PNG
+              source.drawIoWindow.postMessage(JSON.stringify(
+                {action: 'export', format: 'xml', spinKey: 'saving'}), '*');
+            }
+            // Received if the export request was processed
+            else if (msg.event == 'export')
+            {
+              // Updates the data URI of the image
+              source.setAttribute('src', msg.data);
+            }
+
+            // Received if the user clicks exit or after export
+            if (msg.event == 'exit' || msg.event == 'export')
+            {
+              // Closes the editor
+              window.removeEventListener('message', receive);
+              source.drawIoWindow.close();
+              source.drawIoWindow = null;
+            }
+          }
+        };
+
+        // Opens the editor
+        window.addEventListener('message', receive);
+        source.drawIoWindow = window.open(url);
+      }
+      else
+      {
+        // Shows existing editor window
+        source.drawIoWindow.focus();
+      }
+    }
+
+})
+
